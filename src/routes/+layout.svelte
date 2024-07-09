03f5b2ce8a90f88635f5417d14f860b8259f53a6
Vann <111390478+Van-x2@users.noreply.github.com>
2024-07-09 09:15:42 +0000

    finished vercel web analytics setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script>
     import "../app.css";
-    import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+    import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit'
     import { inject } from '@vercel/analytics'
 
-injectSpeedInsights();
+inject()
+injectSpeedInsights()
   </script>
   
   <slot />
